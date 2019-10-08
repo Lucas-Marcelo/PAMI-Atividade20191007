fb8e009d6d8bfa74858a3271fa5c090f6bee78c5
Lucas-Marcelo <sonsofmaria@outlook.com>
2019-10-07 22:14:17 -0300

    Exercício 5.

--- app/src/main/java/br/com/etechoracio/atividade/MainActivity.java
+++ app/src/main/java/br/com/etechoracio/atividade/MainActivity.java
@@ -4,9 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity implements CustomDialog.ItemListener{
+public class MainActivity extends AppCompatActivity implements CustomDialog.ItemListener, AdapterView.OnItemLongClickListener{
 
     private boolean insertMode;
     private ItemAdapter adapter;
